3ef5e408e4d8cf4ecfda02bd452bea6dfb546ea9
adamhineiti <79169942+adamhineiti@users.noreply.github.com>
2021-02-25 21:06:02 -0500

    stuff

--- function excersizes/pc/js/pc.js
+++
@@ -1,8 +0,0 @@
-function setup() {
-    createCanvas(800, 600); // canvas size
-    noStroke(); // stops outlines
-    for (let i = 0; i < 230; i++) { // draw circles
-      fill(random(500), random(500), random(500), random(500)); // color lightness
-      ellipse(random(windowWidth), random(windowHeight), random(600)); // code to do with the ellipse itself 
-    }
-  }
